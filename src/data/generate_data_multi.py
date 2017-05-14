bbf7dcc8ac31028b7aa7621009e89b00ed2d9ce2
josh-gree <joshuadouglasgreenhalgh@gmail.com>
2017-05-14 17:12:39 +0100

    multi data gen

---
+++ src/data/generate_data_multi.py
@@ -0,0 +1,39 @@
+import pickle
+import h5py
+
+from utils import make_data
+import sys
+import os
+
+train_path = '../../data/processed/train_multi/'
+val_path = '../../data/processed/val_multi/'
+test_path = '../../data/processed/test_multi/'
+
+os.mkdir(train_path)
+os.mkdir(val_path)
+os.mkdir(test_path)
+
+
+# make train
+for i in range(500):
+    recon, img = make_data_multi()
+    f = h5py.File(train_path + "{}.hdf5".format(i), "w")
+    f.create_dataset('recon', data=recon)
+    f.create_dataset('img', data='img')
+    f.close()
+
+# make validation
+for i in range(100):
+    recon, img = make_data_multi()
+    f = h5py.File(val_path + "{}.hdf5".format(i), "w")
+    f.create_dataset('recon', data=recon)
+    f.create_dataset('img', data='img')
+    f.close()
+
+# make test
+for i in range(500):
+    recon, img = make_data_multi()
+    f = h5py.File(test_path + "{}.hdf5".format(i), "w")
+    f.create_dataset('recon', data=recon)
+    f.create_dataset('img', data='img')
+    f.close()
--- src/data/utils.py
+++ src/data/utils.py
@@ -84,3 +84,46 @@ def make_data():
     mid_energy = img[50, :]
 
     return recon.asarray(), img_min_diff, mid_energy
+
+
+def make_data_multi():
+
+    Es, Is = spectrum(5, 1e7)
+    base = Image.new('RGBA', (Nx, Ny), (0, 0, 0))
+    d = ImageDraw.Draw(base)
+
+    rectx1, recty1, rectx2, recty2 = 0.15, 0.15, 0.85, 0.85
+    rectx1 = rectx1 * Nx
+    recty1 = (1 - recty1) * Ny
+    rectx2 = rectx2 * Nx
+    recty2 = (1 - recty2) * Ny
+
+    rectbbox = [(rectx1, recty1), (rectx2, recty2)]
+    d.rectangle(rectbbox, fill=(1, 0, 0))
+
+    rs, ps, ms = rand_circs()
+    [make_circ(p, r, d, m) for p, r, m in zip([x for x in ps.T], rs, ms)]
+
+    base_array = np.array(base.getdata())
+    material1_array = base_array.reshape(Nx, Ny, 4)[:, :, 0]
+    material2_array = base_array.reshape(Nx, Ny, 4)[:, :, 1]
+
+    outidx = np.where(material1_array == 0)
+    rectidx = np.where(material1_array == 1)
+    midx = [np.where((material1_array == 2) & (material2_array == m))
+            for m in materials]
+
+    img = np.zeros((Ne, Nx, Ny))
+
+    for ind, m in enumerate(midx):
+        img[:, m[0], m[1]
+            ] += np.tile(material_profile[ind, :], (len(m[0]), 1)).T
+
+    img[:, rectidx[0], rectidx[1]] = np.tile(
+        rect_profile, (len(rectidx[0]), 1)).T
+
+    sino = poly_projection(fwd, img, Is)
+
+    recon = fbp(sino)
+
+    return recon.asarray(), img
